f266147fe8052a601a8697562175802b4a648e87
APersonWhoKnowsNothing <fbrittingham04@gmail.com>
2020-04-23 22:18:35 +0000

    d

--- easteregg.py
+++ easteregg.py
@@ -4,3 +4,5 @@ def user_name_check(user_name):
     if user_name.upper() == "THANOS":
         print('A snap is heard, and you are turned to dust.')
         sys.exit()
+    elif user_name.upper() == "EPIC GAMER MOMENT":
+        print('Ah, a true man of culture.')
